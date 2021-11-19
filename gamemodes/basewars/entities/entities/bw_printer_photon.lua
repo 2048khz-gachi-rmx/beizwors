db21e5bae7b616b256fc26862a80be0e88bfc102
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-11-19 20:36:54 +0700

    update lootcrates to main

--- gamemodes/basewars/entities/entities/bw_printer_photon.lua
+++ gamemodes/basewars/entities/entities/bw_printer_photon.lua
@@ -4,9 +4,9 @@ ENT.Base = "bw_base_moneyprinter"
 ENT.PowerRequired = 1000
 ENT.PowerCapacity = 10000
 
-ENT.Capacity 		= 6e9
+ENT.Capacity 		= 12.5e9
 ENT.PrintInterval 	= 1
-ENT.PrintAmount		= 1500000
+ENT.PrintAmount		= 2.6e6
 
 ENT.PrintName = "Photon Printer"
 
@@ -14,4 +14,4 @@ ENT.FontColor = Color(200, 200, 0)
 ENT.BackColor = Color(110,110,110)
 
 ENT.IsValidRaidable = true
-ENT.PresetMaxHealth = 3500
+ENT.PresetMaxHealth = 2500
