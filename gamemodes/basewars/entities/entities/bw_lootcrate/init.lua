d7a637d9f1714b659c2c804893b4a95785418657
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-11-20 18:25:34 +0700

    lootcrate property cycling (for gen)

--- gamemodes/basewars/entities/entities/bw_lootcrate/init.lua
+++ gamemodes/basewars/entities/entities/bw_lootcrate/init.lua
@@ -86,3 +86,107 @@ function ENT:Use(ply)
 
 	self:Remove()
 end
+
+
+
+function ENT:ChangeProperties(ply)
+	local typ = ply:KeyDown(IN_WALK)
+	if typ then
+		for k,v in RandomPairs(self.TypeInfo) do
+			if k ~= self.CrateType then
+				self.CrateType = k
+
+				local models, newSize = table.Random(v.models)
+				self.Size = newSize
+				self.Model = models[math.random(#models)]
+				self:SetModel(self.Model)
+				self:PhysicsInit(SOLID_VPHYSICS)
+				break
+			end
+		end
+
+		self:SetPos(self:GetPos() - self:OBBCenter())
+		return
+	end
+
+	local sz = self.Size
+	local en = 999
+
+	if self.ModelCopies and table.IsEmpty(self.ModelCopies) then
+		local start = 0
+		for k,v in pairs(self.SizeInfo) do
+			if v == sz then
+				start = k
+			end
+		end
+
+		for i=1, #self.SizeInfo + 1 do
+			start = (start % (#self.SizeInfo + 1)) + 1
+			local newSz = self.SizeInfo[start]
+			if self:GetTypeInfo().models[newSz] then
+				sz = newSz
+				break
+			end
+		end
+
+		self.ModelCopies = nil
+	end
+
+	print("picked size:", sz)
+	self.Size = sz
+
+	if not self.ModelCopies then
+		self.ModelCopies = table.Copy(self:GetTypeInfo().models[self.Size])
+		self.ModelNum = 1
+
+		PrintTable(self.ModelCopies)
+	end
+
+	print("model: ", self.ModelNum, "/", self.ModelNum + table.Count(self.ModelCopies) - 1)
+	self.Model = self.ModelCopies[self.ModelNum]
+	self.ModelCopies[self.ModelNum] = nil
+
+	self.ModelNum = self.ModelNum + 1
+
+	self:SetModel(self.Model)
+	self:PhysicsInit(SOLID_VPHYSICS)
+
+	self:SetPos(self:GetPos() - self:OBBCenter())
+end
+
+PermaLootCrates = PermaLootCrates or {}
+
+function ENT:AddPerma()
+	local saveTbl = {
+		self:GetPos(), self:GetAngles(),
+		self.CrateType, self.Size, self:GetModel()
+	}
+
+	if self.SavedKey then
+		PermaLootCrates[self.SavedKey] = saveTbl
+	else
+		local k = table.insert(PermaLootCrates, saveTbl)
+		self.SavedKey = k
+	end
+end
+
+function ENT:RemovePerma()
+	if not self.SavedKey then return end
+	PermaLootCrates[self.SavedKey] = nil
+	self:Remove()
+end
+
+function ENT:CanTool(ply, tr, name, tool)
+	if name == "permaprops" and ply:IsSuperAdmin() then
+		if ply:KeyDown(IN_ATTACK2) then
+			self:RemovePerma()
+		elseif ply:KeyDown(IN_RELOAD) then
+			self:ChangeProperties(ply)
+			self.SavedKey = nil
+		else
+			self:AddPerma()
+		end
+
+		return false
+	end
+end
--- gamemodes/basewars/entities/entities/bw_lootcrate/shared.lua
+++ gamemodes/basewars/entities/entities/bw_lootcrate/shared.lua
@@ -10,27 +10,42 @@ ENT.CanTakeDamage = true
 ENT.NoHUD = true
 ENT.Model = false
 
-ENT.SmallModels = {
-	"models/props_junk/cardboard_box004a.mdl",
-	"models/props_junk/cardboard_box003a.mdl",
+
+ENT.SizeInfo = {
+	"small", "medium", "big", "large"
 }
 
-ENT.MediumModels = {
-	"models/props_junk/cardboard_box003a.mdl", -- overlap intentional
-	"models/props_junk/cardboard_box001a.mdl",
-	"models/props_junk/cardboard_box002a.mdl",
-}
+ENT.TypeInfo = {
+	weapon = {
 
-ENT.SpecialModels = {
-	weapon = "models/props/de_prodigy/ammo_can_02.mdl",
+		models = {
+			medium = {
+				"models/props/de_prodigy/ammo_can_02.mdl",
+			}
+		},
+
+	},
 
 	scraps = {
-		"models/props_c17/BriefCase001a.mdl",
-		"models/props_c17/SuitCase_Passenger_Physics.mdl",
-	}
-	-- nuclear = "models/props/de_train/barrel.mdl",
 
+		models = {
+			small = {
+				"models/props_c17/BriefCase001a.mdl",
+				"models/props_c17/SuitCase_Passenger_Physics.mdl",
+				"models/props/cs_office/Cardboard_box03.mdl",
+				"models/props_junk/cardboard_box004a.mdl",
+			},
+
+			medium = {
+				"models/props_junk/cardboard_box001a.mdl",
+				"models/props_c17/suitcase001a.mdl",
+				"models/Items/item_item_crate.mdl",
+			}
+		},
+
+	},
 }
+
 
 -- todo: models/props/de_train/processor.mdl
 
@@ -51,13 +66,40 @@ function ENT:Ready()
 	self.Ready = true
 end
 
+
+function ENT:SetSpawnInfo(typ, size)
+	if not self.TypeInfo[typ] then
+		errorf("no such type: %s", typ)
+		return
+	end
+
+	if not self.TypeInfo[typ].models[size] then
+		errorf("no such size for type %s: %s", typ, size)
+		return
+	end
+
+	self.CrateType = typ
+	self.Size = size
+end
+
+function ENT:GetTypeInfo()
+	local typ = self.CrateType or "scraps"
+
+	return self.TypeInfo[typ]
+end
+
 function ENT:PreInit()
 	if not self.Inventory and SERVER then
 		error("forgot to create inventory :skull:")
 		return
 	end
 
-	self.Model = self.SmallModels[math.random(#self.SmallModels)]
+
+	self.CrateType = self.CrateType or "scraps"
+	self.Size = self.Size or "small"
+
+	local mTbl = self:GetTypeInfo().models[self.Size]
+	self.Model = mTbl[math.random(#mTbl)]
 end
 
 function ENT:UpdateTransmitState()
