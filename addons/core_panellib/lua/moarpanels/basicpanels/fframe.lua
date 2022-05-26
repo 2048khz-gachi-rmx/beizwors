751494b81e33d75b4eb61caea4e43793c39d3f7e
2048khz-gachi-rmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-05-26 17:24:41 +0700

    PositionPanel for FFrame

--- addons/core_panellib/lua/moarpanels/basicpanels/fframe.lua
+++ addons/core_panellib/lua/moarpanels/basicpanels/fframe.lua
@@ -17,6 +17,22 @@ local close_hov = Color(235, 90, 90)
 local close_unhov = Color(205, 50, 50)
 
 PANEL.RBRadius = 4
+PANEL.HeaderSize = 32
+
+
+
+function PANEL:GetPositioned(pnl)
+	return {self.RBRadius, self.HeaderSize + self.RBRadius}, 	-- pos
+		{self:GetWide() - self.RBRadius * 2, 		-- size
+		self:GetTall() - self.HeaderSize - self.RBRadius * 2}
+end
+
+function PANEL:PositionPanel(pnl)
+	local pos, sz = self:GetPositioned()
+
+	pnl:SetPos(unpack(pos))
+	pnl:SetSize(unpack(sz))
+end
 
 function PANEL:Init()
 
@@ -57,13 +73,12 @@ function PANEL:Init()
 	self.m_bCloseButton = b
 	self.LabelFont = "OSB24"
 
-	self.HeaderSize = 32
 	self.BackgroundColor = Color(50, 50, 50)
 	self.HeaderColor = Colors.Header:Copy()
 
 	self.DimColor = Color(0, 0, 0, 220)
 
-	self:DockPadding(4, 32 + 4, 4, 4)
+	self:DockPadding(self.RBRadius, self.HeaderSize + self.RBRadius, self.RBRadius, self.RBRadius)
 	self.SizableNum = 3
 
 	self.SizableBoxX = 1
